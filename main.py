4fa1f9fc48074926d47ee4baa5df58330f1cb0db
BGS <gabybgs@ymail.com>
2013-08-16 09:47:39 -0400

    Added even more code o_o

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import shutil
 import os
 import tarfile
 
@@ -22,6 +23,9 @@ class ArchiveExtract(QtCore.QThread):
         self.output_path = opath
 
     def run(self):
+		shutil.rmtree(self.output_path)
+		os.makedirs(self.output_path)
+
 		archive = open(self.path, 'rb')	    
 		tar = tarfile.open(fileobj=archive)
 
@@ -39,17 +43,18 @@ class ArchiveExtract(QtCore.QThread):
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
-        super(QtGui.QWidget, self).__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+		super(QtGui.QWidget, self).__init__(parent)
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
 
-        self.setupSignals()
+		self.setupSignals()
 
-        self.arch_path = "/rogentos/"
-        self.package_list = None
-        self.output_dir = None
+		self.arch_path = "/rogentos/"
+		self.package_list = None
+		self.output_dir = None
+		self.chroot_dir = None
 
-        self.init_info()
+		self.init_info()
 
                     
     def setupSignals(self):
@@ -60,54 +65,57 @@ class MainWindow(QtGui.QMainWindow):
 
     def init_info(self):
          self.ui.textarea.append("<b><font size=5>Rogentos Chroot Deployment Tool</font></b><br>")
-         self.ui.textarea.append("<b><font color=blue> #! Archive path is:</font> {0}</b>".format(self.arch_path)) 
-         self.ui.textarea.append("<b><font color=blue> #! Package List path is:</font> {0} <font color=red>(Required)</font></b>".format(self.package_list))
-         self.ui.textarea.append("<b><font color=blue> #! Output dir path is:</font> {0} <font color=red>(Required)</font></b></b>".format(self.output_dir))
+         self.ui.textarea.append("<b><font color=blue> <font color=black>#!</font> Archive path is:</font> {0}</b>".format(self.arch_path)) 
+         self.ui.textarea.append("<b><font color=blue> <font color=black>#!</font> Package List path is:</font> {0} <font color=red>(Required)</font></b>".format(self.package_list))
+         self.ui.textarea.append("<b><font color=blue> <font color=black>#!</font> Output dir path is:</font> {0} <font color=red>(Required)</font></b></b>".format(self.output_dir))
         
     
     def archive_file_dialog(self):
         arch_path = QtGui.QFileDialog.getOpenFileName(self, 'Archive Path', '.')
         if arch_path:
-            self.ui.textarea.append("<b><font color=blue> >> Archive path:</font> {0}</b>".format(arch_path))
+            self.ui.textarea.append("<b><font color=blue> <font color=black>>></font> Archive path:</font> {0}</b>".format(arch_path))
             self.arch_path = arch_path
 
     def package_file_dialog(self):
         package_path = QtGui.QFileDialog.getOpenFileName(self, 'Package List Path', '.')
         if package_path:
-            self.ui.textarea.append("<b><font color=blue> >> Package List path:</font> {0}</b>".format(package_path))
+            self.ui.textarea.append("<b><font color=blue> <font color=black>>></font> Package List path:</font> {0}</b>".format(package_path))
             self.package_list = package_path
 
     def working_dir_path(self):
         working_dir_path = QtGui.QFileDialog.getExistingDirectory(self, 'Working Directory Path')
         if working_dir_path:
-            self.ui.textarea.append("<b><font color=blue> >> Output dir path:</font> {0}</b>".format(working_dir_path))
-            self.output_dir = working_dir_path
+			self.ui.textarea.append("<b><font color=blue> <font color=black>>></font> Output dir path:</font> {0}</b>".format(working_dir_path))
+			self.ui.textarea.append("<b><font color=black> <font color=red>!!</font> Warning folder content will be <font color=red>erased</font></font>!</b>")
+			self.output_dir = working_dir_path
             
     def chroot_setup(self, status):
         if status == 0:
-			self.ui.textarea.append("<b><font color=black> >> Extraction finished <font color=green>successefully</font>!</font></b>")
+			self.ui.textarea.append("<b><font color=black> >> Extraction feinished <font color=green>successefully</font>!</font></b>")
 			self.ui.statusbar.showMessage("#! Extraction complete!", 15)
 			self.ui.textarea.append("<b><font color=black>#! Checking <font color=red>extracted</font> files!</font></b>")
+			
+			for name in os.listdir(self.output_dir):
+				if os.path.isdir(os.path.join(self.output_dir, name)):
+					self.chroot_dir = os.path.join(self.output_dir, name)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			if os.path.isdir(self.chroot_dir+"/dev"):
+				self.ui.textarea.append("<b><font color=black>>> Found <font color=green>dev</font> !</font></b>")
+			else:
+				self.ui.textarea.append("<b><font color=black>>> Cannot find <font color=red>dev</font> please check the archive!</font></b>")
+				return
+			if os.path.isdir(self.chroot_dir+"/proc"):
+				self.ui.textarea.append("<b><font color=black>>> Found <font color=green>proc</font> !</font></b>")
+			else:
+				self.ui.textarea.append("<b><font color=black>>> Cannot find <font color=red>proc</font> please check the archive!</font></b>")
+				return
+			if os.path.isdir(self.chroot_dir+"/sys"):
+				self.ui.textarea.append("<b><font color=black>>> Found <font color=green>sys</font> !</font></b>")
+			else:
+				self.ui.textarea.append("<b><font color=black>>> Cannot find <font color=red>sys</font> please check the archive!</font></b>")
+				return
+			
+			self.ui.textarea.append("<b><font color=black> #! Setting up <font color=green>Chroot</font> environment!<b></font>")
 
         else:
              self.ui.textarea.append("<b><font color=red> >> Something went wrong while extracting files!</font></b>")
@@ -121,24 +129,28 @@ class MainWindow(QtGui.QMainWindow):
 			pass
  
     def begin_execution(self):
-        if self.package_list == None and self.output_dir == None:
-            self.ui.textarea.append("<b><font color=black>!! Please input <font color=red>Package List</font> path and <font color=red>Output Dir</font> path!</font></b>")
-            return
-        if os.path.isfile(self.arch_path) == False:
-            self.ui.textarea.append("<b><font color=black>!! Invalid <font color=red>Archive</font> file!<b></font>")
-            return
-        if os.path.isfile(self.package_list) == False:
-            self.ui.textarea.append("<b><font color=black>!! Invalid <font color=red>Package List</font> file!</b>")
-            return
-        if os.path.isdir(self.output_dir) == False:
-            self.ui.textarea.append("<b><font color=black>!! Invalid <font color=red>Output</font> directory!</b>")
-            return
+		if self.package_list == None and self.output_dir == None:
+			self.ui.textarea.append("<b><font color=black><font color=red>!!</font> Please input <font color=red>Package List</font> path and <font color=red>Output Dir</font> path!</font></b>")
+			return
+		if os.path.isfile(self.arch_path) == False:
+			self.ui.textarea.append("<b><font color=black><font color=red>!!</font> Invalid <font color=red>Archive</font> file!<b></font>")
+			return
+		if os.path.isfile(self.package_list) == False:
+			self.ui.textarea.append("<b><font color=black><font color=red>!!</font> Invalid <font color=red>Package List</font> file!</b>")
+			return
+		if os.path.isdir(self.output_dir) == False:
+			self.ui.textarea.append("<b><font color=black><font color=red>!!</font> Invalid <font color=red>Output</font> directory!</b>")
+			return
+		
         
-        self.ui.textarea.append("<b><font color=green> #! Starting deployment!</font></b>")
-        self.archive_worker = ArchiveExtract(self.arch_path, self.output_dir)
-        self.archive_worker.sigProgress[str,int].connect(self.progress_callback)
-        self.archive_worker.sigFinished[int].connect(self.chroot_setup)
-        if not self.archive_worker.isRunning():
+		self.ui.textarea.append("<b><font color=green> #! Starting deployment!</font></b>")
+		self.archive_worker = ArchiveExtract(self.arch_path, self.output_dir)
+		self.archive_worker.sigProgress[str,int].connect(self.progress_callback)
+		self.archive_worker.sigFinished[int].connect(self.chroot_setup)
+		
+
+		
+		if not self.archive_worker.isRunning():
 			self.ui.textarea.append("<b><font color=black> #! Archive <font color=green>Decompression</font> started!</font></b>")
 			self.archive_worker.start()
 			self.ui.textarea.append("<b><font color=black> >> Scanning archive <font color=green>files</font>, please wait.<font></b>")
